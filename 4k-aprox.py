5ff7012c4219a3994fdc8e59427902ed74bace76
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-04 15:19:45 -0600

    [ADD] Algoritmo por aproximación

--- 4k-aprox.py
+++ 4k-aprox.py
@@ -10,10 +10,10 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # +-----------------------------+
 # Constants
 X_0 = 0
-Y_0 = 2
+Y_0 = -3
 
 # Steps
-H = 0.2
+H = 1
 H_HR= 0.01
 
 # Number of iterations
@@ -22,7 +22,7 @@ N = 5
 # +-----------------------------+
 # Functions
 def f(x, y):
-    return x - y
+    return x + y / 5
 
 def euler_step(x_i, y_i, h, f):
     return y_i + h*f(x_i, y_i)
@@ -33,31 +33,6 @@ def rk4_step(x_i, y_i, h, f):
     k3 = f(x_i + h/2, y_i + k2*h/2)
     k4 = f(x_i + h, y_i + k3*h)
     return y_i + h/6*(k1 + 2*k2 + 2*k3 + k4)
-
-def exact_solution(x):
-    return x - 1 + 3 / math.exp(x)
-
-# +-----------------------------+
-# Computing high resolution exact result
-x = X_0
-
-xs_exact_hr = [X_0]
-ys_exact_hr = [Y_0]
-
-while x < ((H * N) + X_0):
-    x += H_HR
-    xs_exact_hr.append(x)
-    ys_exact_hr.append(exact_solution(x))
-
-# +-----------------------------+
-# Computing exact results
-x = X_0
-
-ys_exact_p = [Y_0]
-
-for i in range(N):
-    x += H
-    ys_exact_p.append(exact_solution(x))
 
 # +-----------------------------+
 # Computing approximate results with Euler
@@ -84,37 +59,6 @@ for i in range(N):
     x += H
 
 # +-----------------------------+
-# Computing errors
-x = X_0
-
-rk_abs_errs = []
-rk_rel_errs = []
-rk_rel_errs_p = []
-
-euler_abs_errs = []
-euler_rel_errs = []
-euler_rel_errs_p = []
-
-for i in range(N + 1):
-    rk_abs_err = abs(ys_exact_p[i] - ys_rk4[i])
-    rk_rel_err = rk_abs_err / ys_exact_p[i]
-    rk_rel_err_p = rk_rel_err * 100
-    
-    euler_abs_err = abs(ys_exact_p[i] - ys_euler[i])
-    euler_rel_err = euler_abs_err / ys_exact_p[i]
-    euler_rel_err_p = euler_rel_err * 100
-    
-    rk_abs_errs.append(rk_abs_err)
-    rk_rel_errs.append(rk_rel_err)
-    rk_rel_errs_p.append(rk_rel_err_p)
-    
-    euler_abs_errs.append(euler_abs_err)
-    euler_rel_errs.append(euler_rel_err)
-    euler_rel_errs_p.append(euler_rel_err_p)
-    
-    x += H
-
-# +-----------------------------+
 # Computing x values
 x = X_0
 
@@ -137,18 +81,10 @@ for i in range(N):
 # print(len(euler_rel_errs_p))
 # Info tables
 data_rk = {'x': xs,
-        'y(Exacta)': ys_exact_p,
-        'y(RK4)': ys_rk4,
-        'Err abs': rk_abs_errs,
-        'Er rel': rk_rel_errs,
-        'Err rel %': rk_rel_errs_p}
+        'y(RK4)': ys_rk4}
 
 data_euler = {'x': xs,
-        'y(Exacta)': ys_exact_p,
-        'y(Euler)': ys_euler,
-        'Err abs': euler_abs_errs,
-        'Err rel': euler_rel_errs,
-        'Err rel %': euler_rel_errs_p}
+        'y(Euler)': ys_euler}
 
 df_rk = pd.DataFrame(data_rk)
 df_euler = pd.DataFrame(data_euler)
@@ -167,8 +103,6 @@ txt_area.pack(expand=True, fill=tk.BOTH)
 # Plotting
 plt.plot(xs, ys_rk4, color='red', marker='o', linewidth=0.0, label='RK4')
 plt.plot(xs, ys_euler, color='green', marker='o', linewidth=0.0, label='Euler')
-plt.plot(xs, ys_exact_p, color='blue', marker='x', linewidth=0.0, label='Exact')
-plt.plot(xs_exact_hr, ys_exact_hr, color='black', label='Exact')
 plt.legend()
 
 # Show the graphic individualy
